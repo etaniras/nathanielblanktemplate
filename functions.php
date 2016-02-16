8f30148d5c933d6fd20a5a622d4e168544a58b03
etaniras <etaniras@gmail.com>
2016-02-16 09:43:57 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,21 +2,21 @@
 
 /*------------------------Enable Widgets-------------------------*/
 
-function blank_template_widgets() {
+function blank_widgets_init() {
 	register_sidebar( array(
 		'name' => ('First Widget'),
 		'id' => 'first-widget',
 		'description' => 'Widget for our sidebar on pages',
-		'before_widget' => '<div class="widget-sidebar">',
+		'before_widget' => '<div class="widget-sidebar widget-footer">',
 		'after_widget' => '</div>',
 		'before_title' => '<h3>',
 		'after_title' => '</h3>'
 	));
 		register_sidebar( array(
 		'name' => ('Footer left'),
-		'id' => 'first-widget',
-		'description' => 'Widget for our sidebar on pages',
-		'before_widget' => '<div class="widget-sidebar">',
+		'id' => 'footer-left',
+		'description' => 'Widget for the left side of our footer',
+		'before_widget' => '<div class="widget-sidebar widget-footer">',
 		'after_widget' => '</div>',
 		'before_title' => '<h3>',
 		'after_title' => '</h3>'
@@ -26,27 +26,31 @@ function blank_template_widgets() {
 		'name' => ('Footer Middle'),
 		'id' => 'first-widget',
 		'description' => 'Widget for our sidebar on pages',
-		'before_widget' => '<div class="widget-sidebar">',
+		'before_widget' => '<div class="widget-sidebar widget-footer">',
 		'after_widget' => '</div>',
 		'before_title' => '<h3>',
 		'after_title' => '</h3>'
 	));
 		register_sidebar( array(
 		'name' => ('Footer right'),
-		'id' => 'first-widget',
-		'description' => 'Widget for our sidebar on pages',
-		'before_widget' => '<div class="widget-sidebar">',
+		'id' => 'footer-right',
+		'description' => 'Widget for the right side of our footer',
+		'before_widget' => '<div class="widget-sidebar widget-footer">',
 		'after_widget' => '</div>',
 		'before_title' => '<h3>',
 		'after_title' => '</h3>'
 	));
-	)
 }
-add_action('widgets_init', 'blank_widgets_init');
+add_action('widgets_init', 'blank_widgets_init'); /* add_action('widgets_init', 'blank_widgets_init'); */
 
 /*------------------------Enable Menu-------------------------*/
 
 add_theme_support('menus');
+
+/*---- Enable Post Thumbnails ----*/
+
+add_theme_support( 'post-thumbnails' );
+
 
 
 ?>
